a973bdfaaf4f034e50bcc48a1e6d039e323cf6b0
slurpeesz <khtbee@gmail.com>
2023-05-02 10:12:21 -0700

    auth page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,6 +1,12 @@
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../utils/mutations";
+import Register from "../Register/Register"
+import Grid from '@mui/material/Grid';
+import InputLabel from "@mui/material/InputLabel";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import TextField from "@mui/material/TextField";
 
 import Auth from "../../utils/auth.js";
 
@@ -9,6 +15,7 @@ function Login() {
     email: "",
     password: "",
   });
+
 
   const [Login, { error, data }] = useMutation(LOGIN_USER);
 
@@ -30,24 +37,43 @@ function Login() {
     }
   };
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleFormSubmit}>
-        <input
-          type="email"
-          name="email"
-          value={formState.email}
-          onChange={handleInputChange}
-        />
-        <input
-          type="password"
-          name="password"
-          value={formState.password}
-          onChange={handleInputChange}
-        />
-        <button type="submit">Login User</button>
-      </form>
-    </div>
+    <>
+
+        <Grid container justifyContent="center">
+          <Grid container justifyContent="center">
+            <h1>Login Here</h1>
+          </Grid>
+          <Grid item justifyContent="center" xs={10} md={6} lg={4}>
+            <form onSubmit={handleFormSubmit}>
+              <InputLabel>Email</InputLabel>
+              <TextField 
+                type="email"
+                name="email"
+                value={formState.email}
+                fullWidth
+                onChange={handleInputChange}
+              />
+              <InputLabel>Password</InputLabel>
+              <TextField
+                type="password"
+                name="password"
+                value={formState.password}
+                fullWidth
+                onChange={handleInputChange}
+              />
+              {/* <button type="submit">Login User</button> */}
+              <Stack justifyContent="center" direction="row" spacing={2} padding={2}>
+                <Button variant="contained" color="success">
+                  Login User
+                </Button>
+              </Stack>
+            </form>
+          </Grid>
+
+          <Register />
+        </Grid>
+
+    </>
   );
 }
 

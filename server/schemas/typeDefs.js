9ac2eef6fa97f9bbb21341d56ca14e101d4a8652
dirtydenny <dallen7@socal.rr.com>
2023-05-02 13:38:02 -0700

    reolvers and typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,9 +8,34 @@ const typeDefs = gql`
     password: String
   }
 
+  type Recipe {
+    _id: ID
+    name: String
+    ingredients: [String]
+    instructions: [String!]
+    region: String!
+    cookTime: Int
+    image: String
+    description: String
+  }
+
   type Auth {
     token: ID!
     user: User
+  }
+
+  input RecipeInput {
+    name: String
+    ingredients: [String]
+    instructions: [String!]
+    region: String!
+    cookTime: Int
+    image: String
+    description: String
+  }
+
+  type DeleteObj {
+    example: String
   }
 
   type Query {
@@ -20,6 +45,8 @@ const typeDefs = gql`
   type Mutation {
     register(name: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    saveRecipe(recipeData: RecipeInput!): Recipe
+    removeRecipe(id: ID!): DeleteObj
   }
 `;
 

074461db4575eb10940395847e47c55d6f39c40c
PintoDrop <mikebender22@gmail.com>
2023-05-02 13:46:54 -0700

    recipe card showing data

--- client/src/pages/Recipes/Recipes.js
+++ client/src/pages/Recipes/Recipes.js
@@ -56,123 +56,108 @@ function Recipes() {
       cookTime: "30 minutes",
       description: "Tacos made with pollo for a zesty punch",
 
-      image: require("../../Images/chickenTacos.jpg"),
+      image: require("../../Images/chickTacos.jpg"),
     },
-    // {
-    //   name: "Apple Walnut Salad",
-    //   ingredients:
-    //     "Arugula, Belgian Endive, Apples, Dried Cranberries, Blue Cheese, Nuts, Maple Cinnamon Dressing  ",
-    //   instructions:
-    //     "1. Toast and chop the walnuts. 2.Whisk the dressing ingredients together. Stir in the apples. 3. Place the arugula and endive in a bowl. 4. Add part of the cranberries, cheese, and walnuts. 5. Transfer the apples to the bowl with the greens. Toss to coat. Continue to add the dressing until the salad is moist. 6.Finish with the remaining toppings and toss. ENJOY!",
-    //   region: "American",
-    //   cookTime: "28 minutes",
-    //   description: "Crisp salad",
+    {
+      name: "Apple Walnut Salad",
+      ingredients:
+        "Arugula, Belgian Endive, Apples, Dried Cranberries, Blue Cheese, Nuts, Maple Cinnamon Dressing  ",
+      instructions:
+        "1. Toast and chop the walnuts. 2.Whisk the dressing ingredients together. Stir in the apples. 3. Place the arugula and endive in a bowl. 4. Add part of the cranberries, cheese, and walnuts. 5. Transfer the apples to the bowl with the greens. Toss to coat. Continue to add the dressing until the salad is moist. 6.Finish with the remaining toppings and toss. ENJOY!",
+      region: "American",
+      cookTime: "28 minutes",
+      description: "Crisp salad",
 
-    //   image: require("../../Images/appleWalnutSal.jpg"),
-    // },
+      image: require("../../Images/appleWalnutSal.jpg"),
+    },
   ]);
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            {/* user avatar goes here */}R
-          </Avatar>
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        // need to define models
-        title={recipes.name}
-        // subheader="September 14, 2016"
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image={recipes.image}
-        // alt="Paella dish"
-      />
-      <CardContent>
-        <Typography variant="body2" color="text.secondary">
-          {/* need description model */}
-          {/* {recipes.map((recipe, idx) => ( */}
-          <h2>{recipes.description} </h2>
-          {recipes.description}
+    <div style={{ display: "flex", flexDirection: "row" }}>
+      {recipes.map((recipe) => (
+        <Card sx={{ maxWidth: 345 }} key={recipe.name}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                {/* user avatar goes here */}R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={recipe.name}
+            // subheader="September 14, 2016"
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            // needs to be src not image?
+            src={recipe.image}
+            // alt="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              <h2>Description: </h2>
+              <p>{recipe.description}</p>
 
-          <h2>Directions: </h2>
-          {recipes.directions}
+              <h2>Directions: </h2>
+              <p>{recipe.instructions}</p>
 
-          <h2>Ingredients: </h2>
-          {recipes.ingredients}
+              <h2>Ingredients: </h2>
+              <p>{recipe.ingredients}</p>
 
-          <h3>Cook time: </h3>
-          {recipes.cookTime}
+              <h3>Cook time: </h3>
+              <p>{recipe.cookTime}</p>
 
-          <h3>Region: </h3>
-          {recipes.region}
-        </Typography>
-      </CardContent>
-       {/* ))} */}
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            {/* need directions model */}
-            <h2>Directions: </h2>
-            {recipes.directions}
-            {/* Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-            aside for 10 minutes. */}
-          </Typography>
-          <Typography paragraph>
-            <h2>Ingredients: </h2>
-            {recipes.ingredients}
-            {/* Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over
-            medium-high heat. Add chicken, shrimp and chorizo, and cook, stirring
-            occasionally until lightly browned, 6 to 8 minutes. Transfer shrimp to a
-            large plate and set aside, leaving chicken and chorizo in the pan. Add
-            pimentón, bay leaves, garlic, tomatoes, onion, salt and pepper, and cook,
-            stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil. */}
-          </Typography>
-          <Typography paragraph>
-            <h3>Cook time: </h3>
-            {recipes.cookTime}
-            {/* Add rice and stir very gently to distribute. Top with artichokes and
-            peppers, and cook without stirring, until most of the liquid is absorbed,
-            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
-            mussels, tucking them down into the rice, and cook again without
-            stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don&apos;t open.) */}
-          </Typography>
-          <Typography>
-            <h3>Region: </h3>
-            {recipes.region}
-            {/* Set aside off of the heat to let rest for 10 minutes, and then serve. */}
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+              <h3>Region: </h3>
+              <p>{recipe.region}</p>
+            </Typography>
+          </CardContent>
+
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                <h2>Directions: </h2>
+                {recipe.directions}
+              </Typography>
+              <Typography paragraph>
+                <h2>Ingredients: </h2>
+                {recipe.ingredients}
+              </Typography>
+              <Typography paragraph>
+                <h3>Cook time: </h3>
+                {recipe.cookTime}
+              </Typography>
+              <Typography>
+                <h3>Region: </h3>
+                {recipe.region}
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      ))}
+    </div>
   );
-}
+        }
 
 // function Recipe() {
   

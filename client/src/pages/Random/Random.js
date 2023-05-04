4f15581cc72081a439783719f6019beb9a8fb472
MaJo <mateuskingjoo@gmail.com>
2023-05-04 13:35:20 -0700

    test2

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -9,45 +9,21 @@ function Random() {
   // replace with data from API
   const recipes = ["chicken tacos"];
   const [randoRecipe, setRandoRecipe] = useState();
- */
 
-function RandomRecipePicker() {
-  const [recipes, setRecipes] = useState([]);
-  const [selectedCookingStyle, setSelectedCookingStyle] = useState(''); 
-  const [randomRecipe, setRandomRecipe] = useState(null);
-
-  // filter recipe 
-  const handlePickRandomRecipe = () => {
-    // filtering the recipe to only "style/region" that we picked
-    const filteredRecipes = recipes.filter(
-      (recipe) => recipe.cookingStyle === selectedCookingStyle
-    );
-    // picking the random recipe
-    const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
-    const randomRecipe = filteredRecipes[randomIndex];
-    // setting the recipe
-    setRandomRecipe(randomRecipe);
+  const handleClick = () => {
+    var randomIndex = Math.floor(Math.random() * recipes.length);
+    setRandoRecipe(recipes[randomIndex]);
   };
-
-
-/*   const handleClick = () => {
-      var randomIndex = Math.floor(Math.random() * recipes.length);
-      setRandoRecipe(recipes[randomIndex]);
-    
-  }; */
   //   const randoIndex = Math.floor(math.random() * recipe.length);
 
   return (
+    // <>
     <div>
       <Grid container justifyContent="center">
         <h1>Random Recipe</h1>
       </Grid>
       <Grid container justifyContent="center">
-        <Button
-          variant="contained"
-          color="success"
-          onClick={handlePickRandomRecipe}
-        >
+        <Button variant="contained" color="success" onClick={handleClick}>
           Find Random Recipe
         </Button>
       </Grid>
@@ -59,19 +35,19 @@ function RandomRecipePicker() {
   );
 }
 
-export default RandomRecipePicker;
+export default Random;
 
 //     data
-// 
-// 
-// 
-// 
-// 
+//
+//
+//
+//
+//
 // render(Recipe)
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+//
+//
+//
+//
+//
+//
+//

54c77de28ed51f5f0c14819da5aca8d0fd119cd0
MaJo <mateuskingjoo@gmail.com>
2023-05-05 10:47:44 -0700

    random recipe logic

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -4,6 +4,75 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
 
+const CookingStyleDropdown = ({ styles, onStyleSelected }) => {
+  const [selectedStyle, setSelectedStyle] = useState('');
+
+  const handleSelectChange = (event) => {
+    const selectedStyle = event.target.value;
+    setSelectedStyle(selectedStyle);
+    onStyleSelected(selectedStyle);
+  };
+
+  return (
+    <div>
+      <label htmlFor="style-dropdown">Select a cooking style: </label>
+      <select id="style-dropdown" value={selectedStyle} onChange={handleSelectChange}>
+        <option value="">-- Please select --</option>
+        {styles.map((style) => (
+          <option key={style} value={style}>
+            {style}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+const RecipePicker = ({ recipes }) => {
+  const [randomRecipes, setRandomRecipes] = useState([]);
+
+  const handleStyleSelected = (selectedStyle) => {
+    const filteredRecipes = recipes.filter(
+      (recipe) => recipe.cookingStyle.toLowerCase() === selectedStyle.toLowerCase()
+    );
+    if (filteredRecipes.length === 0) {
+      alert("No recipes found for the specified cooking style.");
+      return;
+    }
+    const randomIndexes = [];
+    while (randomIndexes.length < 3 && randomIndexes.length < filteredRecipes.length) {
+      const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
+      }
+    }
+    const randomRecipes = randomIndexes.map((index) => filteredRecipes[index]);
+    setRandomRecipes(randomRecipes);
+  };
+
+  const styles = Array.from(new Set(recipes.map((recipe) => recipe.cookingStyle)));
+
+  return (
+    <div>
+      <CookingStyleDropdown styles={styles} onStyleSelected={handleStyleSelected} />
+      {randomRecipes.length > 0 && (
+        <div>
+          <h3>Random Recipes:</h3>
+          <ul>
+            {randomRecipes.map((recipe) => (
+              <li key={recipe.name}>{recipe.name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+
+
+
+
 
 function Random() {
 

0e2b8b6c0df01b293a35feaebaf39b1970d9cf32
MaJo <mateuskingjoo@gmail.com>
2023-05-06 12:31:46 -0700

    random recipe logic

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -1,47 +1,47 @@
-// import React, { useState } from "react";
-// import Recipes from "../Recipes";
-// import Button from "@mui/material/Button";
-// import Grid from "@mui/material/Grid";
-// import { useQuery } from "@apollo/client";
-// import { QUERY_RECIPES } from "../../utils/queries";
+import React, { useState } from "react";
+import Recipes from "../Recipes";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { useQuery } from "@apollo/client";
+import { QUERY_RECIPES } from "../../utils/queries";
 
-// const RegionDropdown = ({ regions, onRegionSelected }) => {
-//   const [selectedRegion, setSelectedRegion] = useState("");
+const RegionDropdown = ({ regions, onRegionSelected }) => {
+  const [selectedRegion, setSelectedRegion] = useState("");
 
-//   const handleSelectChange = (event) => {
-//     const selectedRegion = event.target.value;
-//     setSelectedRegion(selectedRegion);
-//     onRegionSelected(selectedRegion);
-//   };
+  const handleSelectChange = (event) => {
+    const selectedRegion = event.target.value;
+    setSelectedRegion(selectedRegion);
+    onRegionSelected(selectedRegion);
+  };
 
-//   return (
-//     <div>
-//       <label htmlFor="region-dropdown">Select a region: </label>
-//       <select
-//         id="region-dropdown"
-//         value={selectedRegion}
-//         onChange={handleSelectChange}
-//       >
-//         <option value=""> Please select </option>
-//         {regions.map((region) => (
-//           <option key={region} value={region}>
-//             {region}
-//           </option>
-//         ))}
-//       </select>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <label htmlFor="region-dropdown">Select a region: </label>
+      <select
+        id="region-dropdown"
+        value={selectedRegion}
+        onChange={handleSelectChange}
+      >
+        <option value=""> Please select </option>
+        {regions.map((region) => (
+          <option key={region} value={region}>
+            {region}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
 
-// const RandomRecipePicker = () => {
-//   const { loading, data } = useQuery(QUERY_RECIPES);
-//   const [randomRecipes, setRandomRecipes] = useState([]);
-//   const [showDropdown, setShowDropdown] = useState(false);
-//   const [selectedRegion, setSelectedRegion] = useState("");
+const RandomRecipePicker = () => {
+  const { loading, data } = useQuery(QUERY_RECIPES);
+  const [randomRecipes, setRandomRecipes] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState("");
 
-//   const handleRegionSelected = (selectedRegion) => {
-//     setSelectedRegion(selectedRegion);
-//   };
+  const handleRegionSelected = (selectedRegion) => {
+    setSelectedRegion(selectedRegion);
+  };
 
   const handleRecipePick = () => {
     if (!selectedRegion) {
@@ -51,35 +51,35 @@
 
     if (!data) return;
 
-//     const recipes = data.recipes;
+    const recipes = data.recipes;
 
     const filteredRecipes = recipes.filter(
       (recipe) => recipe.region.toLowerCase() === selectedRegion.toLowerCase()
     );
 
-//     if (filteredRecipes.length === 0) {
-//       alert("No recipes found for the specified region.");
-//       return;
-//     }
+    if (filteredRecipes.length === 0) {
+      alert("No recipes found for the specified region.");
+      return;
+    }
 
-//     const randomIndexes = [];
-//     while (
-//       randomIndexes.length < 3 &&
-//       randomIndexes.length < filteredRecipes.length
-//     ) {
-//       const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
-//       if (!randomIndexes.includes(randomIndex)) {
-//         randomIndexes.push(randomIndex);
-//       }
-//     }
-//     const randomRecipes = randomIndexes.map((index) => filteredRecipes[index]);
-//     setRandomRecipes(randomRecipes);
-//   };
+    const randomIndexes = [];
+    while (
+      randomIndexes.length < 3 &&
+      randomIndexes.length < filteredRecipes.length
+    ) {
+      const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
+      }
+    }
+    const randomRecipes = randomIndexes.map((index) => filteredRecipes[index]);
+    setRandomRecipes(randomRecipes);
+  };
 
-//   if (loading) return <div>Loading...</div>;
+  if (loading) return <div>Loading...</div>;
 
-//   const recipes = data?.recipes || [];
-//   const regions = Array.from(new Set(recipes.map((recipe) => recipe.region)));
+  const recipes = data?.recipes || [];
+  const regions = Array.from(new Set(recipes.map((recipe) => recipe.region)));
 
   return (
     <div>

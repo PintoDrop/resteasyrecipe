8993ddc293c2ff46698753cdf470936e1d2f0c26
PintoDrop <mikebender22@gmail.com>
2023-05-07 17:52:07 -0700

    fixing random recipe

--- client/src/pages/Random/Random.js
+++ client/src/pages/Random/Random.js
@@ -5,14 +5,14 @@ import Grid from "@mui/material/Grid";
 import { useQuery } from "@apollo/client";
 import { QUERY_RECIPES } from "../../utils/queries";
 
-// const RegionDropdown = ({ regions, onRegionSelected }) => {
-//   const [selectedRegion, setSelectedRegion] = useState("");
+const RegionDropdown = ({ regions, onRegionSelected }) => {
+  const [selectedRegion, setSelectedRegion] = useState("");
 
-//   const handleSelectChange = (event) => {
-//     const selectedRegion = event.target.value;
-//     setSelectedRegion(selectedRegion);
-//     onRegionSelected(selectedRegion);
-//   };
+  const handleSelectChange = (event) => {
+    const selectedRegion = event.target.value;
+    setSelectedRegion(selectedRegion);
+    onRegionSelected(selectedRegion);
+  };
 
   return (
     <>
@@ -37,54 +37,51 @@ import { QUERY_RECIPES } from "../../utils/queries";
   );
 };
 
-// const RandomRecipePicker = () => {
-//   const { loading, data } = useQuery(QUERY_RECIPES);
-//   const [randomRecipes, setRandomRecipes] = useState([]);
-//   const [showDropdown, setShowDropdown] = useState(false);
-//   const [selectedRegion, setSelectedRegion] = useState("");
+const RandomRecipePicker = () => {
+  const { loading, data } = useQuery(QUERY_RECIPES);
+  const [randomRecipes, setRandomRecipes] = useState([]);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedRegion, setSelectedRegion] = useState("");
 
-//   const handleRegionSelected = (selectedRegion) => {
-//     setSelectedRegion(selectedRegion);
-//   };
+  const handleRegionSelected = (selectedRegion) => {
+    setSelectedRegion(selectedRegion);
+  };
 
-  // const handleRecipePick = () => {
-  //   if (!selectedRegion) {
-  //     alert("Please select a region.");
-  //     return;
-  //   }
-    
-  //   if (!data) return;
+  const handleRecipePick = () => {
+    if (!selectedRegion) {
+      alert("Please select a region.");
+      return;
+    }
 
-  //   const recipes = data.recipes;
+    if (!data) return;
+    const recipes = data.recipes;
+    const filteredRecipes = recipes.filter(
+      (recipe) => recipe.region.toLowerCase() === selectedRegion.toLowerCase()
+    );
 
-  //   const filteredRecipes = recipes.filter(
-  //     (recipe) =>
-  //       recipe.region.toLowerCase() === selectedRegion.toLowerCase()
-  //   );
+    if (filteredRecipes.length === 0) {
+      alert("No recipes found for the specified region.");
+      return;
+    }
 
-//     if (filteredRecipes.length === 0) {
-//       alert("No recipes found for the specified region.");
-//       return;
-//     }
+    const randomIndexes = [];
+    while (
+      randomIndexes.length < 3 &&
+      randomIndexes.length < filteredRecipes.length
+    ) {
+      const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
+      }
+    }
+    const randomRecipes = randomIndexes.map((index) => filteredRecipes[index]);
+    setRandomRecipes(randomRecipes);
+  };
 
-//     const randomIndexes = [];
-//     while (
-//       randomIndexes.length < 3 &&
-//       randomIndexes.length < filteredRecipes.length
-//     ) {
-//       const randomIndex = Math.floor(Math.random() * filteredRecipes.length);
-//       if (!randomIndexes.includes(randomIndex)) {
-//         randomIndexes.push(randomIndex);
-//       }
-//     }
-//     const randomRecipes = randomIndexes.map((index) => filteredRecipes[index]);
-//     setRandomRecipes(randomRecipes);
-//   };
+  if (loading) return <div>Loading...</div>;
 
-//   if (loading) return <div>Loading...</div>;
-
-//   const recipes = data?.recipes || [];
-//   const regions = Array.from(new Set(recipes.map((recipe) => recipe.region)));
+  const recipes = data?.recipes || [];
+  const regions = Array.from(new Set(recipes.map((recipe) => recipe.region)));
 
   return (
     <>
@@ -105,9 +102,8 @@ import { QUERY_RECIPES } from "../../utils/queries";
         )}
       </Grid>
 
-
       {randomRecipes.length > 0 && (
-        <Grid container justifyContent="center" spacing={4} >
+        <Grid container justifyContent="center" spacing={4}>
           {randomRecipes.map((recipe) => (
             <Grid item xs={12} sm={6} md={4} key={recipe._id}>
               <RecipeCard recipe={recipe} />
@@ -119,17 +115,4 @@ import { QUERY_RECIPES } from "../../utils/queries";
   );
 };
 
-//     data
-//
-//
-//
-//
-//
-// render(Recipe)
-//
-//
-//
-//
-//
-//
-//
+export default RandomRecipePicker;
